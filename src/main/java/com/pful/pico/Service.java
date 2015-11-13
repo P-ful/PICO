3fd390b5e9e135fdac634ba202a92af5064075d5
youngdocho <imyoungdocho@gmail.com>
2015-11-13 16:05:19 +0900

    Missing files for CRUD Initial commit

--- src/main/java/com/pful/pico/Service.java
+++ src/main/java/com/pful/pico/Service.java
@@ -1,16 +1,46 @@
 package com.pful.pico;
 
+import com.pful.pico.core.Mongo;
+import com.pful.pico.core.RequestRouter;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 
-public class Service extends AbstractVerticle {
+public class Service
+		extends AbstractVerticle
+{
 
-    @Override
-    public void start() {
+	public static final String CONNECTION_STRING = "mongodb://localhost:27017";
+	public static final String DB_NAME = "pico";
+	public static MongoClient mongoClient;
 
-         vertx.createHttpServer().requestHandler(req -> {
-             req.response().end("hello.");
-         }).listen(8080);
-    }
+	@Override
+	public void start()
+	{
+
+		init(vertx);
+
+		Router router = Router.router(vertx);
+
+		router.route().handler(BodyHandler.create());
+		router.post("/entities/:type").handler(RequestRouter::create);
+		router.get("/entities/:id").handler(RequestRouter::read);
+		router.put("/entities/:id").handler(RequestRouter::update);
+		router.delete("/entities/:id").handler(RequestRouter::delete);
+		router.get("/:type/list").handler(RequestRouter::list);
+
+		vertx.createHttpServer()
+		     .requestHandler(router::accept)
+		     .listen(8080);
+	}
+
+	private void init(final Vertx vertx)
+	{
+		mongoClient = Mongo.getInstance(vertx, new JsonObject().put("connection_string", CONNECTION_STRING)
+		                                                       .put("db_name", DB_NAME));
+	}
 
 }

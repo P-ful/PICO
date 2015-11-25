cc0b950bcc67f663319ad3b7fd180f0a97cdb27a
Daeyeon <daeyeon@JooDaeyeons-MacBook-Pro.local>
2015-11-25 16:01:51 +0900

    Skeleton for Finder

--- src/main/java/com/pful/pico/db/Finder.java
+++ src/main/java/com/pful/pico/db/Finder.java
@@ -2,11 +2,15 @@ package com.pful.pico.db;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 /**
  * Created by daeyeon on 11/20/15.
@@ -16,88 +20,144 @@ public class Finder
 	private String collectionName;
 	private Map<String, Object> properties = new HashMap<>();
 
-	public MultipleQuery allOf()
+	private Stack<JsonObject> jsonObjectStack = new Stack<>();
+
+	private JsonObject query = new JsonObject();
+
+	public static Finder open()
 	{
-		return new MultipleQuery();
+		return new Finder();
 	}
 
-	public MultipleQuery anyOf()
+	public BinaryConnector allOf(final JsonObject... jsonObjects)
 	{
-		return new MultipleQuery();
+		checkArgument(jsonObjects.length > 0, "");
+
+		final JsonArray jsonArray = new JsonArray();
+
+		for (final JsonObject jsonObject : jsonObjects) {
+			jsonArray.add(jsonObject);
+		}
+
+		query.put("$and", jsonArray);
+		return new BinaryConnector();
 	}
 
-	public SimpleQuery field(final String name)
+	public BinaryConnector anyOf(final JsonObject... jsonObjects)
 	{
-		return new SimpleQuery(name);
+		for (final JsonObject jsonObject : jsonObjects) {
+
+		}
+
+		return new BinaryConnector();
 	}
 
-	public class MultipleQuery
+	public BinaryConnector noneOf(final JsonObject... jsonObjects)
 	{
-		public MultipleValueSetter field(final String name)
-		{
-			return new MultipleValueSetter(name);
-		}
-
-		public void execute(final Handler<AsyncResult<List<JsonObject>>> resultHandler)
-		{
+		for (final JsonObject jsonObject : jsonObjects) {
 
 		}
 
-		public Executor inCollection(final String name)
+		return new BinaryConnector();
+	}
+
+	public SimpleQueryConnector field(final String name)
+	{
+		return new SimpleQueryConnector(name);
+	}
+
+	public Executor inCollection(final String name)
+	{
+		this.collectionName = name;
+		return new Executor();
+	}
+
+	interface Connector<TReturn>
+	{
+		TReturn is(final Object value);
+
+		TReturn in(final Object value);
+	}
+
+	public static class Field
+	{
+		public static FieldConnector field(final String name)
 		{
-			return new Executor(null);
-		}
-
-		public class MultipleValueSetter
-		{
-			private String fieldName;
-
-			public MultipleValueSetter(final String fieldName)
-			{
-				this.fieldName = fieldName;
-			}
-
-			public MultipleQuery is(final Object value)
-			{
-				return MultipleQuery.this;
-			}
+			return new FieldConnector(name);
 		}
 	}
 
-	public class SimpleQuery
+	public static class FieldConnector
+			implements Connector<JsonObject>
 	{
-
 		private String name;
 
-		public SimpleQuery(final String name)
+		public FieldConnector(final String name)
 		{
 			this.name = name;
 		}
 
-		public Executor is(final Object value)
+		@Override
+		public JsonObject is(final Object value)
+		{
+			return null;
+		}
+
+		@Override
+		public JsonObject in(final Object value)
+		{
+			return null;
+		}
+	}
+
+	public class BinaryConnector
+	{
+		public Finder and()
+		{
+			return Finder.this;
+		}
+
+		public Finder or()
+		{
+			return Finder.this;
+		}
+	}
+
+	public class SimpleQueryConnector
+			implements Connector<Finder>
+	{
+		private String name;
+
+		public SimpleQueryConnector(final String name)
+		{
+			this.name = name;
+		}
+
+		@Override
+		public Finder is(final Object value)
 		{
 			properties.put(name, value);
-			return new Executor(null);
+			return Finder.this;
+		}
+
+		@Override
+		public Finder in(final Object value)
+		{
+			return Finder.this;
 		}
 	}
 
 	public class Executor
 	{
-		private JsonObject query;
-
-		public Executor(final JsonObject query)
-		{
-			this.query = query;
-		}
-
 		public void execute(final Handler<AsyncResult<List<JsonObject>>> resultHandler)
 		{
+			final JsonObject query = new JsonObject();
 
-		}
+			properties.forEach((key, value) -> {
+				query.put(key, value);
+			});
 
-		public Executor inCollection(final String name)
-		{
-			return this;
+			MongoDB.mongoClientSingleton.find(collectionName, query, resultHandler);
 		}
 	}
 }
--- src/test/java/com/pful/pico/db/FinderTest.java
+++ src/test/java/com/pful/pico/db/FinderTest.java
@@ -1,46 +1,94 @@
 package com.pful.pico.db;
 
+import com.pful.pico.Service;
+import io.vertx.core.Vertx;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+import static com.pful.pico.db.Finder.Field.field;
 
 /**
  * Created by daeyeon on 11/20/15.
  */
 public class FinderTest
 {
+	private static Vertx vertx;
+
+	@BeforeClass
+	public static void startMongoDB()
+	{
+		vertx = Vertx.vertx();
+		vertx.deployVerticle(Service.class.getName());
+	}
+
+	@AfterClass
+	public static void stopMongoDb()
+	{
+		vertx.close();
+	}
+
 	@Test
 	public void simpleQueryTest()
 	{
-		final Finder builder = new Finder();
-		builder.field("A").is("a")
-		       .inCollection("A")
-		       .execute((results) -> {
+		final CountDownLatch latch = new CountDownLatch(1);
 
+		final Finder builder = new Finder();
+		builder.field("name").is("111")
+		       .inCollection("Entities")
+		       .execute((results) -> {
+			       System.out.println(results.result());
+			       latch.countDown();
 		       });
+
+		try {
+			latch.await();
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
-	public void allOfQueryTest()
+	public void simpleQueryTest2()
 	{
-		final Finder builder = new Finder();
-		builder.allOf()
-		       .field("key1").is("value1")
-		       .field("key2").is("value2")
-		       .inCollection("A")
-		       .execute((results) -> {
+		Finder.open()
+		      .allOf(
+				      field("a").is("F"),
+				      field("b").is("V"))
+		      .and().field("").is("A")
+		      .field("c").is("G")
+		      .inCollection("")
+		      .execute(result -> {
 
-		       });
+		      });
 	}
 
-	@Test
-	public void anyOfQueryTest()
-	{
-		final Finder builder = new Finder();
-		builder.anyOf()
-		       .field("key1").is("value1")
-		       .field("key2").is("value2")
-		       .inCollection("A")
-		       .execute((results) -> {
-
-		       });
-	}
+//	@Test
+//	public void allOfQueryTest()
+//	{
+//		final Finder builder = new Finder();
+//		builder.allOf()
+//		       .field("key1").is("value1")
+//		       .field("key2").is("value2")
+//		       .inCollection("A")
+//		       .execute((results) -> {
+//
+//		       });
+//	}
+//
+//	@Test
+//	public void anyOfQueryTest()
+//	{
+//		final Finder builder = new Finder();
+//		builder.anyOf()
+//		       .field("key1").is("value1")
+//		       .field("key2").is("value2")
+//		       .inCollection("A")
+//		       .execute((results) -> {
+//
+//		       });
+//	}
 }
